d28f94fe9638b17d44c535c7dd7fdea79897c05c
EricCode1983 <nuseric1983@gmail.com>
2015-08-25 15:09:20 +0800

    MVCFramework
    For entity code first

---
+++ DataAccessLayer/DatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace DataAccessLayer
+{
+    public class DatabaseSettings
+    {
+        public static void SetDatabase()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SalesERPDAL>());
+           
+         
+        }
+    }
+}
